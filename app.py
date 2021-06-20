5e41414c4078e00ec92db85226e427744bbc8391
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-06-20 18:48:04 +0200

    Add submit_guess route

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
+from model.color import Color
 from model.game import Game
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
+from model.guess import Guess
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'secret string')
 app.jinja_env.filters['zip'] = zip
+app.jinja_env.filters['color'] = Color
 
-game = Game(10, 4, 6, False)
+game = Game(10, 10, 10, False)
 
 
 @app.route('/')
 def home():
+    return render_template('home.html', game=game)
+
+
+@app.route('/guess/', methods=['POST'])
+def submit_guess():
+    game.board.add_guess(Guess(tuple(map(lambda i: Color(int(i)), list(request.form.values())))))
     return render_template('home.html', game=game)
 
 
